501e956ea1c971851e027e93eb0abfb9a4fc0118
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-04-02 23:07:03 +0530

    add change component

--- easy-lab/src/app/routes/profile/profile-routing.module.ts
+++ easy-lab/src/app/routes/profile/profile-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileLayoutComponent } from './profile-layout/profile-layout.component';
 import { ProfileOverviewComponent } from './overview/overview.component';
 import { ProfileSettingsComponent } from './settings/settings.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,10 @@ const routes: Routes = [
         path: 'settings',
         component: ProfileSettingsComponent,
         data: { title: 'Profile Settings' },
+      },
+      {
+        path: 'changepaasword',
+        component: ChangePasswordComponent
       },
     ],
   },
--- easy-lab/src/app/routes/profile/profile.module.ts
+++ easy-lab/src/app/routes/profile/profile.module.ts
@@ -5,13 +5,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileLayoutComponent } from './profile-layout/profile-layout.component';
 import { ProfileOverviewComponent } from './overview/overview.component';
 import { ProfileSettingsComponent } from './settings/settings.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 const COMPONENTS = [ProfileLayoutComponent, ProfileOverviewComponent, ProfileSettingsComponent];
 const COMPONENTS_DYNAMIC = [];
 
 @NgModule({
   imports: [SharedModule, ProfileRoutingModule],
-  declarations: [...COMPONENTS, ...COMPONENTS_DYNAMIC],
+  declarations: [...COMPONENTS, ...COMPONENTS_DYNAMIC, ChangePasswordComponent],
   entryComponents: COMPONENTS_DYNAMIC,
 })
 export class ProfileModule {}
